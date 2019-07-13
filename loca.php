62169301f352743cd00d0a08ba6f1edd6e0d4c4e
bhawna288 <bhawn170122011@iitg.ac.in>
2019-07-13 12:06:33 +0530

    Add files via upload

---
+++ loca.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title></title>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+      <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="style.css">
+      <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" type="text/css" href="
+         https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+      <style type="text/css">
+         .animateuse{
+         animation: leelaanimate 0.5s infinite;
+         }
+         @keyframes leelaanimate{
+         0% { color: red },
+         10% { color: yellow },
+         20%{ color: blue }
+         40% {color: green },
+         50% { color: pink }
+         60% { color: orange },
+         80% {  color: black },
+         100% {  color: brown }
+         }
+      </style>
+   </head>
+   <body>
+
+<div class="container">
+	<h1 class="text-centre text-success top-margin=100px"> WELCOME TO THE WEB_DEVELPOMENT QUIZ</h1><br>
+	
+<div class="col-lg-8 m-auto d-block">
+	<div class="card">
+		<h3 class="text-centre card-header">You have correctly signup </h3> 
+		<h3 class="text-centre card-header">Now you can login and all the best for quiz </h3>
+		<a href="index.php" class="text-centre card-header">login_page </a>
+
+	</div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
